7d9b8017d7ab366c7d39447084ceee405dddad5b
newtonjrfs <newtoljunior@hotmail.com>
2016-10-13 13:29:09 -0300

    Alteracoes nas configuracoes (aba 2)

--- functions/footer.php
+++ functions/footer.php
@@ -18,16 +18,16 @@
     <div class="tab-content">
       <!-- Home tab content -->
       <div class="tab-pane" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <h3 class="control-sidebar-heading">Atividade recente</h3>
         <ul class="control-sidebar-menu">
           <li>
             <a href="javascript:void(0)">
               <i class="menu-icon fa fa-birthday-cake bg-red"></i>
 
               <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+                <h4 class="control-sidebar-subheading">Aniversario do ...</h4>
 
-                <p>Will be 23 on April 24th</p>
+                <p>Dia 30 de novembro</p>
               </div>
             </a>
           </li>
@@ -36,9 +36,9 @@
               <i class="menu-icon fa fa-user bg-yellow"></i>
 
               <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
+                <h4 class="control-sidebar-subheading">Atualizou perfil</h4>
 
-                <p>New phone +1(800)555-1234</p>
+                <p>Novo telefone (62) 39427200</p>
               </div>
             </a>
           </li>
@@ -49,7 +49,7 @@
               <div class="menu-info">
                 <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
 
-                <p>nora@example.com</p>
+                <p>junin@gmail.com</p>
               </div>
             </a>
           </li>
@@ -58,22 +58,22 @@
               <i class="menu-icon fa fa-file-code-o bg-green"></i>
 
               <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
+                <h4 class="control-sidebar-subheading">Execução de 254 trabalhos</h4>
 
-                <p>Execution time 5 seconds</p>
+                <p>Tempo de execução de 5 segundos</p>
               </div>
             </a>
           </li>
         </ul>
         <!-- /.control-sidebar-menu -->
 
-        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <h3 class="control-sidebar-heading">Progresso de tarefas</h3>
         <ul class="control-sidebar-menu">
           <li>
             <a href="javascript:void(0)">
               <h4 class="control-sidebar-subheading">
-                Custom Template Design
-                <span class="label label-danger pull-right">70%</span>
+                Projeto de modelo personalizado
+                <span class="label label-danger pull-right">65%</span>
               </h4>
 
               <div class="progress progress-xxs">
@@ -84,8 +84,8 @@
           <li>
             <a href="javascript:void(0)">
               <h4 class="control-sidebar-subheading">
-                Update Resume
-                <span class="label label-success pull-right">95%</span>
+                Progresso de atualização
+                <span class="label label-success pull-right">85%</span>
               </h4>
 
               <div class="progress progress-xxs">
